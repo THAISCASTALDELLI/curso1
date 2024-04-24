07705f61c526b4b8a8392a6c2c0787cc29425cd1
THAIS CASTALDELLI <118453104+THAISCASTALDELLI@users.noreply.github.com>
2024-04-24 11:27:53 -0300

    r

--- main.js
+++ main.js
@@ -14,14 +14,3 @@ for (let i = 0; i < botoes.length; i++) {
     }
 }
 
-for(let i=0;i <botoes.length;i++){ 
-
-    botoes[i].onclick = function(){
-        
-        for(let j=0;j<botoes.length;j++){  
-            
-            botoes[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo");
-    }
-}
