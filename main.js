98b0b1171d6436902e9fdc3374211629ae644c9a
THAIS CASTALDELLI <118453104+THAISCASTALDELLI@users.noreply.github.com>
2024-05-08 10:51:01 -0300

    tc

--- main.js
+++ main.js
@@ -15,7 +15,15 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+contadores[0].textContent =  calculartempo(tempoObjetivo1);
+
+
+function calculatempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos   = tempoFinal/1000;
+    return segundos;
+}
