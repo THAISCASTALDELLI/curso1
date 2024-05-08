9ad6def2c42bff935dbfb8b4533be585419c8175
THAIS CASTALDELLI <118453104+THAISCASTALDELLI@users.noreply.github.com>
2024-05-08 11:45:28 -0300

    tc

--- main.js
+++ main.js
@@ -23,7 +23,11 @@ const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 const tempos =[tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 contadores[0].textContent =  calculartempo(tempos[0]);
-contadores[1].textContent =  calculartempo(tempoObjetivo1);
+
+for (let i=0; i<contadores.length;i++){
+    contadores[i].textContent = calculaTempo (tempo[i]);
+
+}
 
 
 function calculaTempo(tempoObjetivo) {
